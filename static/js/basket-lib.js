cf8518fac9faee5e43f5047ef5fc4861e6852bbe
Joshua Crunden <joshuacrunden@gmail.com>
2023-04-03 22:35:10 +0100

    Move common code to basket-lib, implement logic for basket

--- static/js/basket-lib.js
+++ static/js/basket-lib.js
@@ -1,6 +1,27 @@
-const CONCESSION_CRITERIA = "Accompanied children under 16";
-const FULL_PRICE_COUNTER_CLASS_NAME = "full-price-counter"
-const CONCESSION_COUNTER_CLASS_NAME = "concession-counter"
+export const CONCESSION_CRITERIA = "Accompanied children under 16";
+export const FULL_PRICE_COUNTER_CLASS_NAME = "full-price-counter"
+export const CONCESSION_COUNTER_CLASS_NAME = "concession-counter"
+
+function counterButtonsClick(e, concertData) {
+	let increment = 1;
+	if(e.target.className === "minus")
+		increment = -1;
+
+	const orderChange = {
+		id: e.target.parentElement.parentElement.className.split('-')[1],
+		fullPriceAdjustment: 0,
+		concessionPriceAdjustment: 0
+	};
+
+	if(e.target.parentElement.className === "full-price")
+		orderChange.fullPriceAdjustment = increment;
+	else
+		orderChange.concessionPriceAdjustment = increment;
+
+	const concerts = updateOrdersInBasket(orderChange, concertData);
+	renderTicketsInBasketCounter(concerts);
+	return;
+}
 
 /**
  * displayError displays an error to the customer if something went wrong when fetching concerts from the API or adding tickets to the basket
@@ -9,4 +30,92 @@ const CONCESSION_COUNTER_CLASS_NAME = "concession-counter"
 export function displayError(err) {
 	console.log(err);
 	return;
+}
+
+/**
+ * getOrdersFromBasket checks the sessionStorage relating to tickets a customer has put in their basket, and returns this data in an object of objects
+ * @returns {object} basketItems - an object of containing objects for each concert, the objects are named by concert ID an contain number of tickets for each type which the customer has put in their basket. If no concerts, an empty object is returned
+*/
+export function getOrdersFromBasket() {
+	const concerts = JSON.parse(sessionStorage.getItem("concerts"));
+	return concerts ? concerts : {};
+}
+
+/**
+ * renderConcert takes a given concert object and renders the information in an HTML block that is injected into the DOM
+ * @param {object} c - a concert contain information like ID, title, description, location, date, ticket data etc.
+ * @returns null
+*/
+export function renderConcert(concertData) {
+	let fullPrice, concession;
+	if(concertData.concessionPrice === 0)
+		concession = "FREE";
+	else
+		concession = `£${concertData.concessionPrice.toString()}`;
+
+	if(concertData.fullPrice === 0)
+		fullPrice = "FREE";
+	else
+		fullPrice = `£${concertData.fullPrice.toString()}`;
+	
+	const div = `
+		<div class=concert-${concertData.id}>
+			<div>${concertData.id}</div>
+			<div>${concertData.title}</div>
+			<div>${concertData.description}</div>
+			<div>${concertData.location}</div>
+			<div class="bob">${concertData.date} ${concertData.time}</div>
+			<!-- <img src=${concertData.imageURL} alt=${concertData.description}> -->
+			<div class="full-price">
+				<span>Adult: ${fullPrice}</span> <button class="minus">-</button><span class="${FULL_PRICE_COUNTER_CLASS_NAME}">0</span><button class="plus">+</button>
+			</div>
+			<div class="concession">
+				<span>${CONCESSION_CRITERIA}: ${concession}</span><button class="minus">-</button><span class="${CONCESSION_COUNTER_CLASS_NAME}">0</span><button class="plus">+</button>
+			</div>
+		</div>
+	`;
+	document.querySelector("#content .container").insertAdjacentHTML("beforeend", div);
+	const buttons = document.querySelectorAll(`.concert-${concertData.id} button`);
+	buttons.forEach(button => button.addEventListener("click", event => counterButtonsClick(event, concertData)));
+	return;
+}
+
+/**
+ * renderTicketsInBasketCounter takes the basket items from the getOrdersFromBasket function call and updates the UI with these values
+ * @param {object} orders - the data returned from getOrdersFromBasket call
+*/
+export function renderTicketsInBasketCounter(orders) {
+	for(const [id, order ] of Object.entries(orders)) {
+		const fullPriceCounter = document.querySelector(`.concert-${id} .${FULL_PRICE_COUNTER_CLASS_NAME}`);
+		if(fullPriceCounter)
+			fullPriceCounter.innerHTML = order.fullPriceCount;
+
+		const concessionCounter = document.querySelector(`.concert-${id} .${CONCESSION_COUNTER_CLASS_NAME}`);
+		if(concessionCounter)
+			concessionCounter.innerHTML = order.concessionPriceCount;
+	}
+}
+
+/**
+ * updateOrdersInBasket updates the orders saved in the sesionStorage
+ * @param {object} order - an object containing the concertID and numerial change to each ticket type
+ * @return {object} concerts - an updated basket
+*/
+export function updateOrdersInBasket(order, concertData) {
+
+	const concerts = getOrdersFromBasket();
+	const existingOrder = concerts[order.id];
+	let newOrder = {...existingOrder};
+
+	// Math.max ensures no minus number
+	newOrder.fullPriceCount = Math.max((existingOrder ? existingOrder.fullPriceCount : 0) + order.fullPriceAdjustment, 0);
+	newOrder.concessionPriceCount = Math.max((existingOrder ? existingOrder.concessionPriceCount : 0) + order.concessionPriceAdjustment, 0);
+
+	if((newOrder.fullPriceCount + newOrder.concessionPriceCount) <= concertData.availableTickets) {
+		concerts[order.id] = newOrder;
+		sessionStorage.setItem("concerts", JSON.stringify(concerts));
+	} else {
+		displayError("no more tickets are available");
+	}
+	return concerts;
 }
--- static/js/basket.js
+++ static/js/basket.js
@@ -1,3 +1,70 @@
-import { displayError } from "./tickets.js";
+import {
+	getOrdersFromBasket,
+	renderConcert,
+	renderTicketsInBasketCounter,
+} from "./basket-lib.js";
 
-console.log("basket.js connected");
+async function main() {
+	const orders = getOrdersFromBasket();
+	const concerts = await getConcertsInfoFromOrders(orders);
+	for (const concert of concerts) {
+		renderConcert(concert);
+		document.querySelectorAll(`.concert-${concert.id} button`).forEach(button => button.addEventListener("click", removeIfEmpty));
+		addDeleteButtons(orders, concert);
+	}
+	renderTicketsInBasketCounter(orders);
+}
+
+function addDeleteButtons(orders) {
+	for(const [id, _ ] of Object.entries(orders)) {
+		const concertCard = document.querySelector(`.concert-${id}`);
+		concertCard.insertAdjacentHTML("beforeend", "<button class=\"delete\">X</button>");
+		const deleteButton = concertCard.querySelector(".delete");
+		deleteButton.addEventListener("click", e => removeItemFromBasket(e.target.parentElement.className.split('-')[1]));
+	}
+}
+
+async function getConcertFromOrder(orderId) {
+	let result;
+	try {
+		const url = `https://api.philomusica.org.uk/concerts?id=${orderId}`;
+		const response = await fetch(url, {
+			method: "GET",
+			headers: {
+				"Accept": "application/json"
+			},
+		});
+		result = await response.json();
+	} catch (e) {
+		console.log("error calling api", e);
+	}
+	return result;
+}
+
+async function getConcertsInfoFromOrders(orders) {
+	let concerts = new Array();
+	for (const [id] of Object.entries(orders)) {
+		concerts.push(await getConcertFromOrder(id));
+	}
+	return concerts;
+}
+
+function removeIfEmpty(e) {
+	const orderId = e.target.parentElement.parentElement.className.split('-')[1];
+	const concert = getOrdersFromBasket()[orderId];
+	if(concert.fullPriceCount === 0 && concert.concessionPriceCount === 0)
+		removeItemFromBasket(orderId);
+	return;
+}
+	
+
+function removeItemFromBasket(orderId) {
+	let concerts = getOrdersFromBasket();
+	delete concerts[orderId];
+	sessionStorage.setItem("concerts", JSON.stringify(concerts));
+	const order = document.querySelector(`.concert-${orderId}`);
+	order.remove();
+	return;
+}
+
+main();
