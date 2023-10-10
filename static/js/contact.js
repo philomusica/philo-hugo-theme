1d3f25126f8196cf9deee8b644aae188fcfdd958
Joshua Crunden <joshuacrunden@gmail.com>
2023-10-10 22:36:09 +0100

    Migrate from reCAPTCHA v2 to reCAPTCHA v3

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,79 +1,80 @@
 /*
-    CONTACT
+	CONTACT
 */
 function clearForm(name, email, message) {
-    name.value = "";
-    email.value = "";
-    message.value = "";
+	name.value = "";
+	email.value = "";
+	message.value = "";
 }
 
 function feedbackToUser(result) {
-    if(result === "success") {
-        const successMessage = document.querySelector(".alert-success");
-        successMessage.classList.remove("transparent");
-    } else if (result === "fail") {
-        const failMessage = document.querySelector(".alert-fail");
-        failMessage.classList.remove("transparent");
-    } else if (result === "no-captcha") {
-        const failMessage = document.querySelector(".alert-warn");
-        failMessage.classList.remove("transparent");
-    }
-    setTimeout(clearAlert, 5000);
+	if (result === "success") {
+		const successMessage = document.querySelector(".alert-success");
+		successMessage.classList.remove("transparent");
+	} else if (result === "fail") {
+		const failMessage = document.querySelector(".alert-fail");
+		failMessage.classList.remove("transparent");
+	} else if (result === "no-captcha") {
+		const failMessage = document.querySelector(".alert-warn");
+		failMessage.classList.remove("transparent");
+	}
+	setTimeout(clearAlert, 5000);
 }
 
 function clearAlert() {
-    const alerts = document.querySelectorAll(".alert");
-    alerts.forEach(alert => {
-        if(!alert.classList.contains("transparent")) {
-            alert.classList.add("transparent");
-        }
-    });
+	const alerts = document.querySelectorAll(".alert");
+	alerts.forEach(alert => {
+		if (!alert.classList.contains("transparent")) {
+			alert.classList.add("transparent");
+		}
+	});
 }
 
 /* eslint-disable */
 function processForm(e) {
-/* eslint-enable */
-    e.preventDefault();
-    clearAlert();
-    var URL = "https://api.philomusica.org.uk/contact-us";
+	/* eslint-enable */
+	e.preventDefault();
+	clearAlert();
+	var URL = "https://api.philomusica.org.uk/contact-us";
+	/* eslint-disable */
+	grecaptcha.ready(function() {
+		grecaptcha.execute("6LcnzosoAAAAAAkD2T7KwBmLJMglUwCXo8OFzufO", { action: 'submit' }).then(function(token) {
+			/* eslint-enable */
 
-    const name = document.querySelector("input[id=\"name\"]");
-    const email = document.querySelector("input[id=\"email\"]");
-    var message = document.querySelector("textarea[id=\"message\"]");
-    var data = {
-       name : name.value,
-       email : email.value,
-       message : message.value,
-		/* eslint-disable */
-       captchaResponse: grecaptcha.getResponse()
-	   /* eslint-enable */
-    };
+			const name = document.querySelector("input[id=\"name\"]");
+			const email = document.querySelector("input[id=\"email\"]");
+			var message = document.querySelector("textarea[id=\"message\"]");
+			var data = {
+				name: name.value,
+				email: email.value,
+				message: message.value,
+				/* eslint-disable */
+				token: token
+				/* eslint-enable */
+			};
 
-    if(data.captchaResponse === "" || data.captchaResponse === null) {
-        feedbackToUser("no-captcha");
-        return;
-    }
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", URL, true);
+			xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", URL, true);
-    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+			// send the collected data as JSON
+			xhr.send(JSON.stringify(data));
 
-    // send the collected data as JSON
-    xhr.send(JSON.stringify(data));
+			xhr.onload = function() {
+				if (xhr.status === 200) {
+					feedbackToUser("success");
+				} else {
+					feedbackToUser("fail");
+				}
+			};
 
-    xhr.onload = function () {
-        if(xhr.status === 200) {
-            feedbackToUser("success");
-        } else {
-            feedbackToUser("fail");
-        }
-    };
+			xhr.onerror = function() {
+				feedbackToUser("fail");
+			};
 
-    xhr.onerror = function() {
-        feedbackToUser("fail");
-    };
+			clearForm(name, email, message);
 
-    clearForm(name, email, message);
-     
-  }
+		});
+	});
+}
 
